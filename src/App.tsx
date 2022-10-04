0a238f905c745e851d0e3b617ca8c4fc61b6e818
jyc-coder <jyc4648@gmail.com>
2022-10-04 21:33:50 +0900

    #1 - 프로젝트에 불필요한 파일 제거 및 typescript 설치
    프로젝트에 필요하지 않는 CRA 파일을 제거하고 typescript를 설치함

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Antd from './components/Antd';
+import Responsive from './components/Responsive';
+import Slick from './components/Slick';
+function App() {
+  return (
+    <div>
+      <div>Antd</div>
+      <Antd />
+      <div>Responsive</div>
+      <Responsive />
+      <div>Slick</div>
+      <Slick />
+    </div>
+  );
+}
+
+export default App;
