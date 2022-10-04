bfc71780658adcbe76507b15b21405245d776221
jyc-coder <jyc4648@gmail.com>
2022-10-04 21:39:38 +0900

    #1 - responsive 캐러셀 디자인
    responsive 사이트의 example을 바탕으로 캐러셀 디자인을 구현함
    
    https://www.npmjs.com/package/react-responsive-carousel

--- src/components/Responsive.tsx
+++ src/components/Responsive.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const Responsive = () => {
-  return <></>;
+  return (
+    <Carousel infiniteLoop>
+      <div
+        style={{
+          height: 500,
+          background: 'red',
+        }}
+      >
+        hello
+      </div>
+      <div
+        style={{
+          height: 500,
+          background: 'yellow',
+        }}
+      >
+        hello2
+      </div>
+      <div
+        style={{
+          height: 500,
+          background: 'green',
+        }}
+      >
+        hello3
+      </div>
+    </Carousel>
+  );
 };
 
 export default Responsive;
